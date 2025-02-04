3319ddaec13c0cdae2c90612a05f6ca3ec8274b9
Jaiswal <jaiswalrohit55028@gmail.com>
2025-02-04 21:59:15 +0530

    Enquiry form project started

---
+++ UserEnquiryForm/client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="">
+        <h1 className='text-center font-bold'>Welcome to New Project!</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
